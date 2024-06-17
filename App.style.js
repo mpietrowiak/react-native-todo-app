b05170baa15d93dcb443321d4bdd40cc218b0945
Mateusz Pietrowiak <matas8@protonmail.com>
2024-06-17 13:09:25 +0200

    layout

---
+++ App.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  workspace: {
+    height: 450,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  backgroundImg: {
+    height: "100%",
+    // border: "1px solid red",
+  },
+});
